0f49435a7100317b1a839607ada0b134ac6a01ca
Rahul Ranjan <rahul.ranjan@markit.com>
2016-05-30 17:15:44 +0530

    Using express server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var connect = require('connect');
-var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(3000, function(){
-    console.log('Server running on 3000...');
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
+app.listen(4000, function() {
+    console.log('Express server listening on port ' + 4000);
 });
